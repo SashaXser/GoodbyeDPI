13de3c3e298df8297f90c8c6fe43c769b09b00c0
SashaXser <24498484+SashaXser@users.noreply.github.com>
2024-10-08 15:45:56 +0400

    Update goodbyedpi.c

--- src/goodbyedpi.c
+++ src/goodbyedpi.c
@@ -330,42 +330,43 @@ static inline BYTE atoub(const char *str, const char *msg) {
 static HANDLE init(char *filter, UINT64 flags) {
     LPTSTR errormessage = NULL;
     DWORD errorcode = 0;
-    HANDLE handle = WinDivertOpen(filter, WINDIVERT_LAYER_NETWORK, 0, flags);
-    if (handle != INVALID_HANDLE_VALUE)
-        return handle;
-
+    filter = WinDivertOpen(filter, WINDIVERT_LAYER_NETWORK, 0, flags);
+    if (filter != INVALID_HANDLE_VALUE)
+        return filter;
     errorcode = GetLastError();
     FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
                   FORMAT_MESSAGE_IGNORE_INSERTS,
                   NULL, errorcode, MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT),
                   (LPTSTR)&errormessage, 0, NULL);
-    fprintf(stderr, "Error opening filter: %d %s\n", errorcode, errormessage);
+    printf("Error opening filter: %d %s\n", errorcode, errormessage);
     LocalFree(errormessage);
-
-    switch (errorcode) {
-        case ERROR_FILE_NOT_FOUND:
-            fprintf(stderr, "The driver files WinDivert32.sys or WinDivert64.sys were not found.\n");
-            break;
-        case ERROR_DRIVER_ALREADY_LOADED:
-            fprintf(stderr, "An incompatible version of the WinDivert driver is currently loaded.\n"
-                            "Please unload it with the following commands run as administrator:\n\n"
-                            "sc stop windivert\n"
-                            "sc delete windivert\n"
-                            "sc stop windivert14\n"
-                            "sc delete windivert14\n");
-            break;
-        case ERROR_INVALID_IMAGE_HASH:
-            fprintf(stderr, "Could not load driver due to invalid digital signature.\n"
-                            "Please ensure that driver signing is properly configured.\n");
-            break;
-        case ERROR_SERVICE_DISABLED:
-            fprintf(stderr, "The Base Filtering Engine service has been disabled.\n"
-                            "Enable the Base Filtering Engine service.\n");
-            break;
-        default:
-            fprintf(stderr, "An unexpected error occurred while opening the WinDivert handle.\n");
-            break;
-    }
+    if (errorcode == 2)
+        printf("The driver files WinDivert32.sys or WinDivert64.sys were not found.\n");
+    else if (errorcode == 654)
+        printf("An incompatible version of the WinDivert driver is currently loaded.\n"
+               "Please unload it with the following commands ran as administrator:\n\n"
+               "sc stop windivert\n"
+               "sc delete windivert\n"
+               "sc stop windivert14"
+               "sc delete windivert14\n");
+    else if (errorcode == 1275)
+        printf("This error occurs for various reasons, including:\n"
+               "the WinDivert driver is blocked by security software; or\n"
+               "you are using a virtualization environment that does not support drivers.\n");
+    else if (errorcode == 1753)
+        printf("This error occurs when the Base Filtering Engine service has been disabled.\n"
+               "Enable Base Filtering Engine service.\n");
+    else if (errorcode == 577)
+        printf("Could not load driver due to invalid digital signature.\n"
+               "Windows Server 2016 systems must have secure boot disabled to be \n"
+               "able to load WinDivert driver.\n"
+               "Windows 7 systems must be up-to-date or at least have KB3033929 installed.\n"
+               "https://www.microsoft.com/en-us/download/details.aspx?id=46078\n\n"
+               "WARNING! If you see this error on Windows 7, it means your system is horribly "
+               "outdated and SHOULD NOT BE USED TO ACCESS THE INTERNET!\n"
+               "Most probably, you don't have security patches installed and anyone in you LAN or "
+               "public Wi-Fi network can get full access to your computer (MS17-010 and others).\n"
+               "You should install updates IMMEDIATELY.\n");
     return NULL;
 }
 
